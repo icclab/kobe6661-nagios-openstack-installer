5d6679d3f47a56a491b818837e17f696c22bcd3d
kobe6661 <benn@zhaw.ch>
2014-11-04 14:44:51 +0100

    Fixing PEP8 compliance

--- remote_server_config.py
+++ remote_server_config.py
@@ -6,35 +6,47 @@ Created on Tue Oct 28 12:41:21 2014
 """
 import cuisine, pickle
 from fabric.api import env, execute, task, run, sudo, put
-from fabric.contrib.files import sed
+#from fabric.contrib.files import sed
 
-host_list = pickle.load(open('server_list','r'))
+HOST_LIST = pickle.load(open('server_list', 'r'))
 
 #env.hosts = [host[2] for host in host_list if host[0] != u'nagios_test']
-env.hosts=['160.85.4.238']
+env.hosts = ['160.85.4.238']
 env.user = 'ubuntu'
 env.password = 'Ekbn1980!'
 env.key_filename = 'C:\\Users\\Konstantin\\.ssh\\id_rsa'
 print(env.key_filename)
 
 def nagios_downloaded():
+    '''
+    Checks if Nagios is downloaded to VM.
+    '''
     return cuisine.file_exists("~/nagios-3.4.1.tar.gz")
 
 def nagios_plugins_downloaded():
+    '''
+    Checks if Nagios plugins are downloaded to VM.
+    '''
     return cuisine.file_exists("~/nagios-plugins-2.0.3.tar.gz")
-    
+
 def nrpe_plugins_downloaded():
+    '''
+    Checks if NRPE plugins are downloaded to VM.
+    '''
     return cuisine.file_exists("~/nrpe-2.15.tar.gz")
 
 @task
 def add_nagios_user():
+    '''
+    Adds Nagios user and group and sets correct file permissions.
+    '''
     cuisine.group_ensure('nagcmd')
     cuisine.group_ensure('nagios')
     cuisine.user_ensure('nagios')
-    cuisine.group_user_ensure('nagios','nagios')
-    cuisine.group_user_ensure('nagcmd','nagios')
+    cuisine.group_user_ensure('nagios', 'nagios')
+    cuisine.group_user_ensure('nagcmd', 'nagios')
     cuisine.user_ensure('www-data')
-    cuisine.group_user_ensure('nagcmd','www-data')
+    cuisine.group_user_ensure('nagcmd', 'www-data')
     sudo('mkdir -p /usr/local/nagios')
     sudo('mkdir -p /usr/local/nagios/libexec')
     cuisine.dir_ensure('/usr/local/nagios')
@@ -44,6 +56,9 @@ def add_nagios_user():
 
 @task
 def install_prerequisites():
+    '''
+    Installs prerequisites for running Nagios on VM.
+    '''
     cuisine.package_ensure('apache2')
     cuisine.package_ensure('libapache2-mod-php5')
     cuisine.package_ensure('build-essential')
@@ -52,6 +67,9 @@ def install_prerequisites():
 
 @task
 def install_nagios_from_source():
+    '''
+    Downloads Nagios source code and installs Nagios on VM.
+    '''
     cuisine.dir_ensure('/usr/local/src')
     sudo('mkdir -p /etc/httpd/conf.d/')
     cuisine.dir_ensure('/etc/httpd/conf.d/')
@@ -60,27 +78,36 @@ def install_nagios_from_source():
         sudo('wget http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-3.4.1.tar.gz')
     sudo('tar xzf nagios-3.4.1.tar.gz')
     sudo('cd ~/nagios && ./configure --with-command-group=nagcmd && make all && make install')
-    sudo('cd ~/nagios && make install-init && make install-config && make install-commandmode')   
+    sudo('cd ~/nagios && make install-init && make install-config && make install-commandmode')
     sudo('cd ~/nagios && make install-webconf')
     sudo('cp /etc/httpd/conf.d/nagios.conf /etc/apache2/conf-available/nagios.conf')
     sudo('cp /etc/httpd/conf.d/nagios.conf /etc/apache2/conf-enabled/nagios.conf')
     sudo('rm -rf /etc/apache2/conf-available/nagios3.conf')
     sudo('rm -rf /etc/apache2/conf-enabled/nagios3.conf')
-    
+
 @task
 def prepare_apache():
+    '''
+    Creates Nagios admin user and configures Apache.
+    '''
     sudo('htpasswd -bc /usr/local/nagios/etc/htpasswd.users nagiosadmin nagiosadmin')
     sudo('/etc/init.d/apache2 reload')
 
 @task
-def install_nagios_plugins_from_source():    
+def install_nagios_plugins_from_source():
+    '''
+    Downloads Nagios plugins source code and installs Nagios plugins on VM.
+    '''
     if not nagios_plugins_downloaded():
         sudo('wget http://nagios-plugins.org/download/nagios-plugins-2.0.3.tar.gz')
     sudo('tar xzf nagios-plugins-2.0.3.tar.gz')
     sudo('cd ~/nagios-plugins-2.0.3 && ./configure --with-nagios-user=nagios --with-nagios-group=nagios && make && make install')
-    
+
 @task
 def install_nrpe_plugin_from_source():
+    '''
+    Downloads NRPE plugin source code and installs NRPE plugin on VM.
+    '''
     cuisine.package_ensure_apt('libssl-dev')
     cuisine.dir_ensure('/usr/local/src')
     run('cd /usr/local/src')
@@ -91,13 +118,16 @@ def install_nrpe_plugin_from_source():
 
 @task
 def start_nagios():
-    put('./commands.cfg','/usr/local/nagios/etc/objects/commands.cfg',use_sudo=True)
+    '''
+    Starts Nagios on VM and sets up Nagios as upstart job.
+    '''
+    put('./commands.cfg','/usr/local/nagios/etc/objects/commands.cfg', use_sudo=True)
     sudo('ln -sf /etc/init.d/nagios /etc/rcS.d/S99nagios')
     sudo('/etc/init.d/nagios start')
-    
+
 
 execute(add_nagios_user)
-execute(install_prerequisites)    
+execute(install_prerequisites)
 execute(install_nagios_from_source)
 execute(prepare_apache)
 execute(install_nrpe_plugin_from_source)
