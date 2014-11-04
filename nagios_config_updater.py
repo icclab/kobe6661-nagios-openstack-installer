cc69b463b65875e0a33a4e7364a1b585bcbb549b
kobe6661 <benn@zhaw.ch>
2014-11-04 15:01:20 +0100

    Fixing PEP8 compliance

--- nagios_config_updater.py
+++ nagios_config_updater.py
@@ -5,22 +5,25 @@ Created on Tue Oct 28 10:00:29 2014
 @author: Konstantin
 """
 
-import re, os
+import re
 from string import Template as _template
 
 
 def write_config_file(**kwargs):
-    FILE_DIR=kwargs.pop('FILE_DIR','/usr/local/nagios/etc/')
-    TEMPLATE_DIR=kwargs.pop('TEMPLATE_DIR','/usr/local/nagios/etc/')
-    target_config_file = open(str('%snagios.cfg' % FILE_DIR),'w')
-    template_file_name=str('%snagios-template.cfg' % TEMPLATE_DIR)
-    with open(template_file_name,'r') as f:
-        for line in f:
-            if re.search('nagios_config_files',line):
-                s = _template(line)
-                line = s.substitute(nagios_config_files='')
-                with open(str('%sintermediate.txt' % TEMPLATE_DIR),'r') as i:
-                    for temp_line in i:
+    '''
+    Writes updates to Nagios config file from template.
+    '''
+    file_dir = kwargs.pop('file_dir', '/usr/local/nagios/etc/')
+    template_dir = kwargs.pop('template_dir', '/usr/local/nagios/etc/')
+    target_config_file = open(str('%snagios.cfg' % file_dir), 'w')
+    template_file_name = str('%snagios-template.cfg' % template_dir)
+    with open(template_file_name, 'r') as template_file:
+        for line in template_file:
+            if re.search('nagios_config_files', line):
+                param = _template(line)
+                line = param.substitute(nagios_config_files='')
+                with open(str('%sintermediate.txt' % template_dir), 'r') as intermediate:
+                    for temp_line in intermediate:
                         line = temp_line
                         buf = str(line)
                         target_config_file.write(buf)                        
@@ -31,5 +34,5 @@ def write_config_file(**kwargs):
     return target_config_file.name
     
 if __name__ == "__main__":
-    _target_config_file_name = write_config_file()
-    print(_target_config_file_name)
+    _TARGET_CONFIG_FILE_NAME = write_config_file()
+    print(_TARGET_CONFIG_FILE_NAME)
