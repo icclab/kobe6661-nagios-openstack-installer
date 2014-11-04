a27525019a7f3cd1735f7636011ebf0c3b969d14
kobe6661 <benn@zhaw.ch>
2014-11-04 14:32:01 +0100

    Fixing PEP8 compliance

--- vm_list_extractor.py
+++ vm_list_extractor.py
@@ -9,37 +9,39 @@ from novaclient import client as noclient
 import config_transporter, pickle
 
 
-keystoneconn=ksclient.Client(auth_url='http://lisa.cloudcomplab.ch:35357/v2.0', 
-                              username='benn', 
-                              password='icclab123',
-                              tenant='zhaw-users')
+KEYSTONE_CONN = ksclient.Client(
+    auth_url='http://lisa.cloudcomplab.ch:35357/v2.0',
+    username='benn',
+    password='icclab123',
+    tenant='zhaw-users')
 
-print(keystoneconn.authenticate())
+print(KEYSTONE_CONN.authenticate())
 
-os_token = keystoneconn.get_token(keystoneconn.session)
+OS_TOKEN = KEYSTONE_CONN.get_token(KEYSTONE_CONN.session)
 
 
+NOVA_CONN = noclient.Client('1.1',
+                            auth_url='http://lisa.cloudcomplab.ch:35357/v2.0',
+                            username='benn',
+                            auth_token=OS_TOKEN,
+                            tenant_id='36e520370bf742bba3f09690af98b46d')
 
-                        
-novaconn = noclient.Client('1.1', 
-                        auth_url='http://lisa.cloudcomplab.ch:35357/v2.0', 
-                              username='benn', 
-                              auth_token=os_token,
-                              tenant_id='36e520370bf742bba3f09690af98b46d')
+print(NOVA_CONN.servers.list())
 
-print(novaconn.servers.list())
+SERVERS = [(server.name,
+            server.hostId,
+            server.to_dict()['addresses'].values()[0][0]['addr'])
+           for server in NOVA_CONN.servers.list('dict')
+           if server.user_id == u'e87d5f264369457181bf9e374febc04b']
+print(SERVERS)
 
-servers = [(server.name, server.hostId, 
-            server.to_dict()['addresses'].values()[0][0]['addr']) 
-            for server in novaconn.servers.list('dict')
-            if server.user_id == u'e87d5f264369457181bf9e374febc04b']
-print(servers)
+NAGIOS_SERVER = NOVA_CONN.servers.find(name='nagios_test')
 
-nagios_server=novaconn.servers.find(name='nagios_test')
+VM_LIST = [
+    config_transporter.add_file_to_nagios_conf_dir(name=server[0],
+                                                   ip=server[2])
+    for server in SERVERS]
 
-[config_transporter.add_file_to_nagios_conf_dir(name=server[0],
-                                ip=server[2]) for server in servers]
-
-pickle.dump(servers,open('server_list','w'))
+pickle.dump(SERVERS, open('server_list', 'w'))
 
 config_transporter.write_intermediate_file()
